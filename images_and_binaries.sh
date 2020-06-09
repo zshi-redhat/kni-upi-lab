cde3c51543d1ea685cef177d79fe8f83a0de8b39
Zenghui Shi <zshi@redhat.com>
2020-06-09 10:42:50 +0800

    add 4.5, 4.6 release support
    This commit was made based on nfvpe/master

--- images_and_binaries.sh
+++ images_and_binaries.sh
@@ -13,9 +13,9 @@ declare -A RHCOS_BOOT_IMAGES
 # Map of boot type to raw metal image
 declare -A RHCOS_METAL_IMAGES
 
-# 2/6/2020: 4.4 must be acquired from internal Red Hat CI registry, because it is not yet available 
+# 2/6/2020: 4.5/4.6 must be acquired from internal Red Hat CI registry, because it is not yet available 
 # from the official mirror
-if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.4" ]; then
+if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.5" ] || [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.6" ]; then
     BUILDS_JSON="$(curl -sS https://releases-art-rhcos.svc.ci.openshift.org/art/storage/releases/rhcos-$OPENSHIFT_RHCOS_MAJOR_REL/builds.json)"
 
     if [[ -z "$OPENSHIFT_RHCOS_MINOR_REL" ]]; then
@@ -42,7 +42,7 @@ if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.4" ]; then
     RHCOS_METAL_IMAGES["bios"]="$FILENAME"
     RHCOS_METAL_IMAGES["uefi"]="$FILENAME"
 else
-    # 2/6/2020: All versions before 4.4 are available from the official mirror
+    # 2/6/2020: All versions before 4.5/4.6 are available from the official mirror
 
     OPENSHIFT_RHCOS_MINOR_REL="$(curl -sS https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/$OPENSHIFT_RHCOS_MAJOR_REL/latest/ | grep rhcos-$OPENSHIFT_RHCOS_MAJOR_REL | head -1 | cut -d '-' -f 2)"
 
@@ -56,8 +56,8 @@ else
     # Now handle metal images and map file names to sha256 values
     FILENAME_LIST=("${RHCOS_BOOT_IMAGES["kernel"]}" "${RHCOS_BOOT_IMAGES["ramdisk"]}")
     
-    if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.3" ]; then
-        # 4.3 uses one unified metal image 
+    if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.3" ] || [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.4" ]; then
+        # 4.3/4.4 uses one unified metal image 
 
         UNIFIED_METAL="$(echo "$SHA256" | grep x86_64-metal | rev | cut -d ' ' -f 1 | rev)"
         
@@ -108,12 +108,16 @@ export RHCOS_METAL_IMAGES
 
 # 4.4 is a special case, and requires getting the latest version ID from an index page
 LATEST_4_4="$(curl -sS https://openshift-release-artifacts.svc.ci.openshift.org/ | awk "/4\.4\./ && !(/s390x/ || /ppc64le/)" | tail -1 | cut -d '"' -f 2)"
+LATEST_4_5="$(curl -sS https://openshift-release-artifacts.svc.ci.openshift.org/ | awk "/4\.5\./ && !(/s390x/ || /ppc64le/)" | tail -1 | cut -d '"' -f 2)"
+LATEST_4_6="$(curl -sS https://openshift-release-artifacts.svc.ci.openshift.org/ | awk "/4\.6\./ && !(/s390x/ || /ppc64le/)" | tail -1 | cut -d '"' -f 2)"
 
 declare -A OCP_BINARIES=(
     [4.1]="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.1/"
     [4.2]="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.2/"
     [4.3]="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.3/"
-    [4.4]="https://openshift-release-artifacts.svc.ci.openshift.org/$LATEST_4_4"
+    [4.4]="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.4/"
+    [4.5]="https://openshift-release-artifacts.svc.ci.openshift.org/$LATEST_4_5"
+    [4.6]="https://openshift-release-artifacts.svc.ci.openshift.org/$LATEST_4_6"
 )
 
 # TODO: remove debug
@@ -126,8 +130,8 @@ OCP_INSTALL_BINARY_URL=""
 
 FIELD_SELECTOR=8
 
-if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.4" ]; then
-    # HACK: 4.4 has a different HTML structure than the rest
+if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.5" ] || [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.6" ]; then
+    # HACK: 4.5/4.6 has a different HTML structure than the rest
     FIELD_SELECTOR=2
 fi
 
@@ -146,7 +150,7 @@ fi
 export OCP_CLIENT_BINARY_URL
 export OCP_INSTALL_BINARY_URL
 
-if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.4" ] || [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.5" ]; then
+if [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.4" ] || [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.5" ] || [ "$OPENSHIFT_RHCOS_MAJOR_REL" == "4.6" ]; then
 	ENABLE_BOOTSTRAP_BOOT_INDEX="true"
 else
 	ENABLE_BOOTSTRAP_BOOT_INDEX="false"
--- scripts/gen_ignition.sh
+++ scripts/gen_ignition.sh
@@ -108,7 +108,7 @@ patch_manifest() {
         cp_manifest "$PROJECT_DIR/cluster/standalone/manifest" "$ocp_dir/manifests"
 
 	# TODO remove this workaround for 4.4 deployment
-	if [[ $OPENSHIFT_RHCOS_MAJOR_REL == "4.4" ]]; then
+	if [ $OPENSHIFT_RHCOS_MAJOR_REL == "4.4" ] || [ $OPENSHIFT_RHCOS_MAJOR_REL == "4.5" ] || [ $OPENSHIFT_RHCOS_MAJOR_REL == "4.6" ]; then
 		printf "Overwriting 02_autoapprover_statefulset.yaml for 4.4 StatefulSet API change"
 		cp -f "$PROJECT_DIR/cluster/standalone/openshift/4.4/02_autoapprover_statefulset-4.4.yaml" \
 			"$ocp_dir/openshift/02_autoapprover_statefulset.yaml"
